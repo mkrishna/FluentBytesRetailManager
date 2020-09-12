1c8c71ef43eb977a1b84d4d338c2b713f25d4be1
Krishna praveen <mkrishnapraveen@hotmail.com>
2020-09-13 00:55:16 +0530

    Add the Web API Project
    Adds a blank Web API Project and tweaks it

---
+++ FBRMDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FBRMDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ FBRMDataManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FBRMDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
